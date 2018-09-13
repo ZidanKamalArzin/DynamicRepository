e004ad0bb40c3c443eb5252c6184b8f167dfa9ad
WIN 10 <WIN 10@DESKTOP-MN29TUB>
2018-09-13 13:03:14 +0700

    Initial Commit

---
+++ src/Jobsheet07/HitungFibonacci.java
@@ -0,0 +1,19 @@
+
+package Jobsheet07;
+
+public class HitungFibonacci {
+    private static void tampilJudul(String identitas)
+    {
+        System.out.println("Identitas : "+ identitas);
+        
+        System.out.println("\nHitung Fibonacci");
+        System.out.println("1, 1, 2, 3, 5, 8, 13, 21, ... dst.\n");
+    }
+    public static void main(String[] args )
+    {
+        String identitas = "Zidan Kamal Arzin / X RPL 2 / 40";
+    
+        tampilJudul(identitas);
+    }
+  
+}
